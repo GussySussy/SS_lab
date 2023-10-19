ffacf66298dbdb3f34ce327826c2ed805774ea1e
Gourisankar Shaji <98521330+GussySussy@users.noreply.github.com>
2023-10-19 08:51:54 +0530

    Create sjf.c

---
+++ sjf.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+struct Process {
+    int id;
+    int burst_time;
+};
+
+void sjfScheduling(struct Process processes[], int n) {
+    // Sort processes by burst time (SJF)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (processes[i].burst_time > processes[j].burst_time) {
+                // Swap processes[i] and processes[j]
+                struct Process temp = processes[i];
+                processes[i] = processes[j];
+                processes[j] = temp;
+            }
+        }
+    }
+
+    int waiting_time[n];
+    int turnaround_time[n];
+
+    // Calculate waiting time and turnaround time
+    waiting_time[0] = 0;
+    turnaround_time[0] = processes[0].burst_time;
+
+    for (int i = 1; i < n; i++) {
+        waiting_time[i] = waiting_time[i - 1] + processes[i - 1].burst_time;
+        turnaround_time[i] = waiting_time[i] + processes[i].burst_time;
+    }
+
+    // Display the result in a table
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\n", processes[i].id, processes[i].burst_time, waiting_time[i], turnaround_time[i]);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d",&n);
+
+    struct Process processes[n];
+
+    for (int i = 0; i < n; i++) {
+        processes[i].id = i + 1;
+        printf("Enter burst time for process P%d: ", processes[i].id);
+        scanf("%d", &processes[i].burst_time);
+    }
+
+    sjfScheduling(processes, n);
+
+    return 0;
+}
